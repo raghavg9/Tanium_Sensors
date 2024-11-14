e32d45a80b518fa262e5c8509e33ae94e0a5ea33
raghavg9 <raghavg9@gmail.com>
2024-11-13 22:44:48 -0600

    Update vga.py

--- vga.py
+++ vga.py
@@ -19,5 +19,3 @@ os.system("rm output.txt")
 #Returns graphics card/vga adapter, which is found in the result of the lspci command. Parsing this for VGA will #give the desired card value.
 
 
-#After writing this, I saw that there was a similar implementation of this in the existing Tanium sensors.
-
